8f666b24df7134705d5c6f23ea9a30f738db75d4
Tazheeb Shamsi <94049470+tazheeb-shamsi@users.noreply.github.com>
2022-07-03 12:15:55 +0530

    🔥Final

---
+++ COVID-19 : Tracker/COVID-19 Tracker/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+
+const Footer = () => {
+  return (
+    <div style={{marginTop:"5%",}} >
+      <footer className=" font-small color-dark pt-4">           
+        
+
+        {/* <!-- Copyright --> */}
+        <div className="footer-copyright text-center py-3">
+          © 2022 Copyright:
+          <a style={{textDecoration:"none"}} href="https://tazheeb.com/"> tazheeb.com</a>
+        </div>
+        {/* <!-- Copyright --> */}
+      </footer>
+      
+    </div>
+  );
+};
+
+export default Footer;
